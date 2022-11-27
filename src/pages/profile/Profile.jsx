2ad119fc99b4f01f8d17f639271a5a1477aa93de
argikurnia <argikurnia2@gmail.com>
2022-11-27 20:52:35 +0700

    feat: change fetch data from redux :sparkles:

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import { Container } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { getUserById, historyByUser } from "../../action/fb_database";
@@ -9,18 +10,20 @@ import ProfileHeader from "../../components/profile/ProfileHeader";
 const Profile = () => {
   const [userDataById, setUserDataById] = useState({});
   const [userGameHistory, setUserGameHistory] = useState([]);
-  const { id: playerId } = useParams();
-  console.log(playerId);
+  const userLoginData = useSelector((state) => {
+    return state.userLoginReducer.loginUser;
+  });
   useEffect(() => {
     async function initData() {
-      const userDataById = await getUserById(playerId);
-      const userGameHistoryById = await historyByUser(playerId);
-
-      setUserDataById(userDataById[0]);
-      setUserGameHistory(userGameHistoryById);
+      if (userLoginData[0]) {
+        const userGameHistoryById = await historyByUser(
+          userLoginData[0]?.data?.id_player
+        );
+        setUserGameHistory(userGameHistoryById);
+      }
     }
     initData();
-  }, [playerId]);
+  }, [userLoginData]);
 
   return (
     <>
@@ -35,17 +38,14 @@ const Profile = () => {
             style={{ maxWidth: "1024px", margin: "auto", padding: "9rem 0rem" }}
           >
             <Container>
-              <ProfileHeader user={userDataById} playerId={playerId} />
+              <ProfileHeader />
             </Container>
           </article>
 
           {/* Section Bottom */}
           <article style={{ maxWidth: "1024px", margin: "auto" }}>
             <Container>
-              <ProfileGameHistory
-                userGameHistory={userGameHistory}
-                playerId={playerId}
-              />
+              <ProfileGameHistory userGameHistory={userGameHistory} />
             </Container>
           </article>
         </main>
