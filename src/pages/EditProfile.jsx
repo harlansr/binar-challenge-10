fe29aaf6db29ae74b036efc672fab2dcee19d1e5
argikurnia <argikurnia2@gmail.com>
2022-11-27 20:55:26 +0700

    feat: fixed redux data reload :sparkles:

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -3,17 +3,17 @@ import { Container } from "react-bootstrap";
 import EditProfileBox from "../components/EditProfile/EditProfileBox";
 import Navbar from "../components/layout/nav/Navbar";
 
-class EditProfile extends Component{
-    render (){
-        return(
-            <div>
-                <Navbar/>
-                <Container>
-                    <EditProfileBox/>
-                </Container>
-            </div>
-        )
-    }
+class EditProfile extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Container>
+          <EditProfileBox />
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default EditProfile;
